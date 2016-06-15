f82d634da2aba9a195bc33f53f2e4e279353ce26
Vladimir Keleshev <vladimir@keleshev.com>
2016-06-15 16:07:42 +0200

    Rename Compiler.compile -> Compiler.Term.compile

--- CompilerTest.ml
+++ CompilerTest.ml
@@ -4,33 +4,57 @@ let (test), (=>) = Test.(test, (=>))
 module JS = JavaScript
 module V = Syntax
 
-let compile = Compiler.compile
+let term = Compiler.Term.compile
+let item = Compiler.compile
 
 let () = test "Atoms" @@ fun () ->
-  compile (V.Number 1) => JS.Number 1.;
-  compile (V.String "s") => JS.String "s";
-  compile (V.Identifier "s") => JS.Identifier "s"
+  term (V.Number 1) => JS.Number 1.;
+  term (V.String "s") => JS.String "s";
+  term (V.Identifier "s") => JS.Identifier "s"
 
 let () = test "Case function" @@ fun () ->
-  compile V.(CaseFunction [("parameter", a)])
+  term V.(CaseFunction [("parameter", a)])
     => JS.(Function (None, ["parameter"], [Return a]))
 
 let () = test "If-else" @@ fun () ->
-  compile V.(IfElse (a, b, c))
+  term V.(IfElse (a, b, c))
     => JS.(Call (Function (None, [], [
          IfElse (a, [Return b], [Return c])
        ]), []))
 
 let () = test "Let in" @@ fun () ->
-  compile V.(LetIn ("a", b, c))
+  term V.(LetIn ("a", b, c))
     => JS.(Call (Function (None, ["a"], [
          Return c
        ]), [b]))
 
 let () = test "Infix" @@ fun () ->
-  compile V.(Infix (a, Times, b))
+  term V.(Infix (a, Times, b))
     => JS.(Infix (a, Operator.Times, b))
 
 let () = test "Call" @@ fun () ->
-  compile V.(Call (a, [b; c]))
+  term V.(Call (a, [b; c]))
     => JS.(Call (a, [b; c]))
+
+(* ITEM *)
+
+(*
+let () = test "Module" @@ fun () ->
+  item V.(Module ("foo", []))
+    => JS.(
+*)
+
+
+(*
+
+var foo = {}
+
+
+var foo = (function () {
+  return {};
+})
+
+var foo = (function () {
+  var bar = ...;
+  return {bar: bar};
+})
